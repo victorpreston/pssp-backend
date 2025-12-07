84350d15ef1e073b30cc149c30f49c4869f2c556
Victor Preston <prestonvictor25@gmail.com>
2025-12-07 13:28:26 +0300

    fix(tests): fix async test assertions

--- src/modules/readiness/services/readiness-serializer.service.spec.ts
+++ src/modules/readiness/services/readiness-serializer.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
--- src/modules/readiness/services/readiness.service.spec.ts
+++ src/modules/readiness/services/readiness.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReadinessService } from './readiness.service';
