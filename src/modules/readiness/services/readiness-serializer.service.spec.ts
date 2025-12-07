cc484a095507464c79cf95e04214d0c0910ebefb
Victor Preston <prestonvictor25@gmail.com>
2025-12-07 13:22:43 +0300

    fix(tests): ensure await-thenable eslint rule is properly disabled

--- src/modules/readiness/services/readiness-serializer.service.spec.ts
+++ src/modules/readiness/services/readiness-serializer.service.spec.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReadinessSerializerService } from './readiness-serializer.service';
